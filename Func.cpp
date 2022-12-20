f53becab3ba23d81cb3853d76f9f98d94c12b8f7
Teroro <115535965+xTeroro@users.noreply.github.com>
2022-12-20 10:52:35 +0200

    Tic Tac Toe
    A full working AI vs Player and Player vs Player Tic-Tac-Toe game made by Teroro(Me).

---
+++ Func.cpp
@@ -0,0 +1,186 @@
+#include "Func"
+#include <iostream>
+#include <string>
+#include <random>
+#include <ctime>
+
+using namespace std;
+
+//Question Functions
+bool Func::AskChar(char y, char n, string msg, string error)
+{
+    string input;
+    while (true)
+    {
+        cout << msg << '\n';
+        getline(cin,input);
+        cout<<'\n';
+        if ((input[0] == y || input[0] == y + 32) && input[1] == '\0')
+        {
+            return true;
+        }
+        else if ((input[0] == n || input[0] == n + 32) && input[1] == '\0')
+        {
+            return false;
+        }
+        else
+        {
+            cout << error << '\n';
+        }
+    }
+}
+int Func::Ask3Char(char option_1, char option_2, char r, string msg, string error)
+{
+    string input;
+    while (true)
+    {
+        cout << msg << '\n';
+        getline(cin,input);
+        cout<<'\n';
+        if ((input[0] == option_1 || input[0] == option_1 + 32) && input[1] == '\0')
+        {
+            return 1;
+        }
+        else if ((input[0] == option_2 || input[0] == option_2 + 32) && input[1] == '\0')
+        {
+            return 2;
+        }
+        else if((input[0] == r || input[0] == r + 32) && input[1] == '\0')
+        {
+            return 3;
+        }
+        else
+        {
+            cout << error << '\n';
+        }
+    }
+}
+
+//Resetting Maps Funtions
+void Func::ClearMap(int m[3][3])
+{
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            m[i][j] = 0;
+        }
+    }
+}
+void Func::ResetDrawMap(char m[3][3])
+{
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            m[i][j] = '-';
+        }
+    }
+}
+
+//Input Check for Coordinates
+int Func::CheckCord(int Max, string msg, string error)
+{
+    string input;
+    while (true)
+    {
+        cout<<msg<<'\n';
+        getline(cin,input);
+        cout<<'\n';
+        if(input[0] >= '1' && input[0] <= Max+48 && input[1] == '\0')
+        {
+            return input[0]-49;
+        }else
+        {
+            cout<<error<<'\n';
+        }
+    }
+}
+
+//Check Coordinates
+bool Func::CheckMap(int m[3][3],int cord1,int cord2)
+{
+    if(m[cord1][cord2] == 0)
+    {
+        return true;
+    }else
+    {
+        return false;
+    }
+}
+
+//AI
+int Func::Random(int max)
+{
+    srand(time(NULL));
+    return rand() % max;
+}
+
+//Win Conditions
+bool Func::CheckForEndMap(int m[3][3])
+{
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            if(m[i][j] == 0)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool Func::CheckWin(int m[3][3], char P, char O)
+{
+    for(int i = 0; i < 3; i++)
+    {
+        if(m[i][0] == m[i][1] && m[i][1] == m[i][2] && m[i][0] != 0)
+        {
+            if(m[i][0] == 1)
+            {
+                cout<<P<<" Wins! (slabule)\n";
+                return true;
+            }else
+            {
+                cout<<O<<" Wins! (slabule)\n";
+                return true;
+            }
+        }else if(m[0][i] == m[1][i] && m[1][i] == m[2][i] && m[0][i] != 0)
+        {
+            if(m[0][i] == 1)
+            {
+                cout<<P<<" Wins! (slabule)\n";
+                return true;
+            }else
+            {
+                cout<<O<<" Wins! (slabule)\n";
+                return true;
+            }
+        }else if(m[0][0] == m[1][1] && m[1][1] == m[2][2] && m[1][1] != 0)
+        {
+            if(m[0][0] == 1)
+            {
+                cout<<P<<" Wins! (slabule)\n";
+                return true;
+            }else
+            {
+                cout<<O<<" Wins! (slabule)\n";
+                return true;
+            }
+        }else if(m[0][2] == m[1][1] && m[1][1] == m[2][0] && m[1][1] != 0)
+        {
+            if(m[0][2] == 1)
+            {
+                cout<<P<<" Wins! (slabule)\n";
+                return true;
+            }else
+            {
+                cout<<O<<" Wins! (slabule)\n";
+                return true;
+            }
+        }
+    }
+    return false;
+}
---
+++ main.cpp
@@ -0,0 +1,252 @@
+#include <iostream>
+#include "Func"
+#include "windows.h"
+#include "unistd.h"
+
+using namespace std;
+using namespace Func;
+
+// Core Variables
+char Player, Opponent;
+char DrawMap[3][3];
+int Map[3][3];
+void SetUp()
+{
+    int id;
+    id = Ask3Char('x', '0', 'r', "Select your character X or 0 or R(random) : ", "Invalid character, please try again.");
+    if (id == 1)
+    {
+        Player = 'X';
+        Opponent = '0';
+    }
+    else if(id == 2)
+    {
+        Player = '0';
+        Opponent = 'X';
+    }else
+    {
+        id = Random(2)+1;
+        if(id == 1)
+        {
+            Player = 'X';
+            Opponent = '0';
+        }else
+        {
+            Player = '0';
+            Opponent = 'X';
+        }
+        cout<<"Player character is: "<<Player<<'\n'<<"Opponent character is: "<<Opponent<<"\n\n";
+    }
+    ClearMap(Map);
+    ResetDrawMap(DrawMap);
+}
+
+// Game Variables
+int ColumnCord, RowCord,OpponentCord;
+int BotCards[10], CardsCount;
+void Draw()
+{
+    cout<<"  "<<1<<" "<<2<<" "<<3<<'\n';
+    for (int i = 0; i < 3; i++)
+    {
+        cout<<i+1<<' ';
+        for (int j = 0; j < 3; j++)
+        {
+            cout << DrawMap[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}
+
+void SinglePlayerGame()
+{
+    SetUp();
+    while (true)
+    {
+        Draw();
+        cout << '\n';
+        //Player Turn
+        while (true)
+        {
+            ColumnCord = CheckCord(3, "Enter the row coordinate (from up to down): ", "Invalid coordinate, please try again.");
+            RowCord = CheckCord(3, "Enter the column coordinate (from left to right): ", "Invalid coordinate, please try again.");
+            if (CheckMap(Map, ColumnCord, RowCord) == true)
+            {
+                Map[ColumnCord][RowCord] = 1;
+                DrawMap[ColumnCord][RowCord] = Player;
+                break;
+            }
+            else
+            {
+                cout << "T~his (but a scratch) coordinate is already taken, please try again \n" << '\n';
+            }
+        }
+        //Check for Player Win
+        if(CheckWin(Map, Player, Opponent) == true)
+        {
+            Draw();
+            cout<<'\n';
+            break;
+        }else if(CheckForEndMap(Map) == true)
+        {
+            cout<<"Draw!"<<'\n';
+            Draw();
+            cout<<'\n';
+            break;
+        }
+        //AI Turn
+        for(int i = 0; i <= 9; i++)
+        {
+        BotCards[i] = i;
+        }
+        CardsCount = 9;
+        while(true)
+        {
+        OpponentCord = Random(CardsCount);
+            if(OpponentCord < 3)
+            {
+                ColumnCord = 0;
+                RowCord = OpponentCord;
+            }else if(OpponentCord < 6)
+            {
+                ColumnCord = 1;
+                RowCord = OpponentCord - 3;
+            }else
+            {
+                ColumnCord = 2;
+                RowCord = OpponentCord - 6;
+            }
+            if(CheckMap(Map, ColumnCord, RowCord) == true)
+            {
+                Map[ColumnCord][RowCord] = 2;
+                DrawMap[ColumnCord][RowCord] = Opponent;
+                break;
+            }
+            else
+            {
+                BotCards[OpponentCord] = BotCards[CardsCount];
+                CardsCount--;
+            }
+        }
+        cout<<"The Opponent Choosed: "<<ColumnCord+1<<" and "<<RowCord+1<<'\n';
+        //Check for Opponent Win
+        if(CheckWin(Map, Player, Opponent) == true)
+        {
+            cout<<'\n';
+            Draw();
+            cout<<'\n';
+            break;
+        }else if(CheckForEndMap(Map) == true)
+        {
+            cout<<"Draw!"<<'\n';
+            Draw();
+            cout<<'\n';
+            break;
+        }
+    }
+    if(AskChar('y', 'n', "Do you want to play again? (y/n)", "Invalid character, please try again.") == true)
+    {
+        SinglePlayerGame();
+    }
+}
+void MultiPlayerGame()
+{
+    SetUp();
+    while (true)
+    {
+        //Player Turn
+        Draw();
+        cout << '\n';
+        cout<<"Player Turn"<<'\n';
+        while (true)
+        {
+            ColumnCord = CheckCord(3, "Enter the row coordinate (from up to down): ", "Invalid coordinate, please try again.");
+            RowCord = CheckCord(3, "Enter the column coordinate (from left to right): ", "Invalid coordinate, please try again.");
+            if (CheckMap(Map, ColumnCord, RowCord) == true)
+            {
+                Map[ColumnCord][RowCord] = 1;
+                DrawMap[ColumnCord][RowCord] = Player;
+                break;
+            }
+            else
+            {
+                cout << "T~his (but a scratch) coordinate is already taken, please try again \n" << '\n';
+            }
+        }
+
+        //Check for Player Win
+        if(CheckWin(Map, Player, Opponent) == true)
+        {
+            Draw();
+            cout<<'\n';
+            break;
+        }else if(CheckForEndMap(Map) == true)
+        {
+            cout<<"Draw!"<<'\n';
+            Draw();
+            cout<<'\n';
+            break;
+        }
+        //Player2 Turn
+        Draw();
+        cout << '\n';
+        cout<<"Opponents Turn"<<'\n';
+        while (true)
+        {
+            ColumnCord = CheckCord(3, "Enter the row coordinate (from up to down): ", "Invalid coordinate, please try again.");
+            RowCord = CheckCord(3, "Enter the column coordinate (from left to right): ", "Invalid coordinate, please try again.");
+            if (CheckMap(Map, ColumnCord, RowCord) == true)
+            {
+                Map[ColumnCord][RowCord] = 1;
+                DrawMap[ColumnCord][RowCord] = Opponent;
+                break;
+            }
+            else
+            {
+                cout << "T~his (but a scratch) coordinate is already taken, please try again \n" << '\n';
+            }
+        }
+        //Check for Opponent Win
+        if(CheckWin(Map, Player, Opponent) == true)
+        {
+            cout<<'\n';
+            Draw();
+            cout<<'\n';
+            break;
+        }else if(CheckForEndMap(Map) == true)
+        {
+            cout<<"Draw!"<<'\n';
+            Draw();
+            cout<<'\n';
+            break;
+        }
+    }
+    if(AskChar('y', 'n', "Do you want to play again? (y/n)", "Invalid character, please try again.") == true)
+    {
+        MultiPlayerGame();
+    }
+}
+
+int main()
+{
+    cout << "=======\n|| B ||\n|| Y ||\n||   ||\n|| T ||\n|| E ||\n|| R ||\n|| O ||\n|| R ||\n|| O ||\n======= \n\n\n\n";
+    cout << "Welcome to Tic Tac Toe!\n\n";
+    if (AskChar('c', 'f', "Do you want to play against the computer or a friend? (C/F)", "Please enter a valid input."))
+    { 
+        cout << "You chose to play against the computer.\n\n";
+        SinglePlayerGame();
+    }
+    else
+    {
+        cout << "You chose to play against a friend.\n\n";
+        MultiPlayerGame();
+    }
+
+    //Exit Sequence
+    cout<<"The Program will automatically exit when the timer reaches 0."<<'\n';
+    for(int i = 5; i > 0; i--)
+    {
+        cout<<i<<'\n';
+        sleep(1);
+    }
+}
